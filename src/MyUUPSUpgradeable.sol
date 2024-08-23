ccb265e9db19ea5dce1b2bdbb7ff885023ded54c
BEB 09 박상현 <psh03225@naver.com>
2024-08-23 23:34:30 +0900

    UPDATE: update compiler version

--- src/MyUUPSUpgradeable.sol
+++ src/MyUUPSUpgradeable.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/access/OwnableUpgradeable.sol";
 
 contract MyUUPSUpgradeable is
     Initializable,
